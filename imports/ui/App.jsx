bc47bc19861a541824e65f261626e3e4ade5ec77
levuloi <49064246+levuloihust99@users.noreply.github.com>
2023-04-17 23:23:11 +0700

    done pagination

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor'
 import { useTracker } from 'meteor/react-meteor-data'
-import { Modal, Loader } from "semantic-ui-react"
+import { Modal, Loader, Button } from "semantic-ui-react"
 
 import { RowItem } from './components/RowItem.jsx';
 import { RecordCount, Records } from '../db/records.model.js';
@@ -13,11 +13,11 @@ export const App = () => {
   const { isLoading, records, recordCount } = useTracker(() => {
     const handles = [
       Meteor.subscribe('recordCount'),
-      Meteor.subscribe('paginatedRecords', page, page + PAGE_SIZE)
+      Meteor.subscribe('paginatedRecords', page * PAGE_SIZE, PAGE_SIZE)
     ]
     const isLoading = !handles.every(handle => handle.ready())
     const records = Records.find().fetch()
-    const { count: recordCount } = RecordCount.findOne({ _id: 'number-of-record' }).fetch()
+    const recordCount = RecordCount.findOne({ _id: 'number-of-record' })
     return { isLoading, records, recordCount }
   })
 
@@ -44,16 +44,17 @@ export const App = () => {
   }
 
   const renderPagination = () => {
-    const numPages = Math.floor((recordCount + PAGE_SIZE - 1) / PAGE_SIZE)
+    const numPages = Math.floor((recordCount.count + PAGE_SIZE - 1) / PAGE_SIZE)
     const minPage = Math.max(0, page - 2)
     const maxPage = Math.min(numPages - 1, page + 2)
     let pageButtons = []
     for (let i = minPage; i <= maxPage; i++) {
       if (i == page) {
-        pageButtons.push(<Button primary>{page + 1}</Button>)
+        pageButtons.push(<Button key={`page-button-${i}`} primary>{page + 1}</Button>)
       } else {
         pageButtons.push((
           <Button
+            key={`page-button-${i}`}
             onClick={() => setPage(i)}
           >
             {i + 1}
@@ -63,25 +64,42 @@ export const App = () => {
     }
     if (minPage > 0) {
       const beginButton = (
-        <Button onClick={() => setPage(0)}>begin</Button>
+        <Button key={`page-button-begin`} onClick={() => setPage(0)}>begin</Button>
       )
-      const dots = <span>...</span>
+      const dots = (
+        <span key="dot-after-begin" style={{marginLeft: "10px", marginRight: "10px"}}>
+          . . .
+        </span>
+      )
       pageButtons = [beginButton, dots, ...pageButtons]
     }
     if (maxPage < numPages - 1) {
       const endButton = (
-        <Button onClick={() => setPage(numPages - 1)}>end</Button>
+        <Button key={`page-button-end`} onClick={() => setPage(numPages - 1)}>end</Button>
       )
-      const dots = <span>...</span>
+      const dots = (
+        <span key="dot-before-end" style={{marginLeft: "10px", marginRight: "10px"}}>
+          . . .
+        </span>
+      )
       pageButtons = [...pageButtons, dots, endButton]
     }
-    return pageButtons
+    return (
+      <div className="pagination">
+        {pageButtons}
+      </div>
+    )
   }
 
   return (
     <>
-      {isLoading ? renderLoadingPage() : renderRecords()}
-      {renderPagination()}
+      {isLoading ? 
+        renderLoadingPage() :
+        <>
+          {renderRecords()}
+          {renderPagination()}
+        </>
+      }
     </>
   )
 }
